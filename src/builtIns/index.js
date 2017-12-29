b9ffd04d2ec78abf1d3ed35a66bf4773ab7a1f29
Miklos Bertalan <miklos.bertalan@risingstack.com>
2017-12-29 10:18:42 +0100

    refactor(observable): refactor observable wrapping

---
+++ src/builtIns/index.js
@@ -0,0 +1,26 @@
+import collectionHandlers from './collections'
+
+// simple objects are not wrapped by Proxies, neither instrumented
+const dontInstrument = new Set([
+  Date,
+  RegExp
+])
+
+// built-in object can not be wrapped by Proxies
+// their methods expect the object instance as the 'this' instead of the Proxy wrapper
+// complex objects are wrapped with a Proxy of instrumented methods
+// which switch the proxy to the raw object and to add reactive wiring
+const handlers = new Map([
+  [Map, collectionHandlers],
+  [Set, collectionHandlers],
+  [WeakMap, collectionHandlers],
+  [WeakSet, collectionHandlers],
+])
+
+export function shouldInstrument (obj) {
+  return !dontInstrument.has(obj.constructor)
+}
+
+export function getHandlers (obj) {
+  return handlers.get(obj.constructor)
+}
--- src/observable.js
+++ src/observable.js
@@ -1,14 +1,14 @@
 import { proxyToRaw, rawToProxy } from './internals'
 import { storeObservable } from './store'
-import builtInHandlers from './builtInHandlers'
+import * as builtIns from './builtIns'
 import baseHandlers from './handlers'
 
 export function observable (obj = {}) {
   if (typeof obj !== 'object') {
     throw new TypeError('Observable first argument must be an object or undefined')
   }
-  // if it is already an observable, return it
-  if (proxyToRaw.has(obj)) {
+  // if it is already an observable or it should not be wrapped, return it
+  if (proxyToRaw.has(obj) || !builtIns.shouldInstrument(obj)) {
     return obj
   }
   // if it already has a cached observable wrapper, return it
@@ -17,12 +17,8 @@ export function observable (obj = {}) {
 }
 
 function createObservable (obj) {
-  const handlers = builtInHandlers.get(Object.getPrototypeOf(obj)) || baseHandlers
-  // if it is a simple built-in object, do not wrap it
-  if (handlers === true) {
-    return obj
-  }
   // if it is a complex built-in object or a normal object, wrap it
+  const handlers = builtIns.getHandlers(obj) || baseHandlers
   const observable = new Proxy(obj, handlers)
   // save these to switch between the raw object and the wrapped object with ease later
   rawToProxy.set(obj, observable)
