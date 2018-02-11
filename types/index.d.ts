6f42cdff7bde30315970d3380026d63d81b380ef
Miklos Bertalan <miklos.bertalan@risingstack.com>
2018-02-11 16:44:31 +0100

    feat(typescript): add type definitions

---
+++ types/index.d.ts
@@ -0,0 +1,18 @@
+declare module '@nx-js/observer-util' {
+  function observable<Observable extends object>(obj?: Observable): Observable
+  function isObservable(obj: object): boolean
+  function raw<Observable extends object>(obj: Observable): Observable
+
+  interface Scheduler {
+    add: Function
+    delete: Function
+  }
+
+  interface ObserveOptions {
+    scheduler?: Scheduler | Function
+    lazy?: boolean
+  }
+
+  function observe<Reaction extends Function>(func: Reaction, options?: ObserveOptions): Reaction
+  function unobserve(func: Function): void
+}
