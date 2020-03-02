6cf23744eb996221a52596538b8fac577e3f424f
paranoidjk <hust2012jiangkai@gmail.com>
2020-03-02 15:23:27 +0800

    fix: tsd

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,23 +1,44 @@
 declare module 'nemo-observer-util' {
-  function observable<Observable extends object>(obj?: Observable): Observable
-  function isObservable(obj: object): boolean
-  function raw<Observable extends object>(obj: Observable): Observable
+  function observable<Observable extends object>(obj?: Observable): Observable;
+  function isObservable(obj: object): boolean;
+  function raw<Observable extends object>(obj: Observable): Observable;
 
   interface Scheduler {
-    add: Function
-    delete: Function
+    add: Function;
+    delete: Function;
+  }
+
+  interface ObservableConfig {
+    /**
+     * whether to ignore something like `this.a = this.a`;
+     */
+    skipSameValueChange?: boolean;
+    /**
+     * whether to disable modify outside `@action`
+     */
+    onlyAllowChangeInAction?: boolean;
   }
 
   interface ObserveOptions {
-    scheduler?: Scheduler | Function
-    debugger?: Function
-    lazy?: boolean
+    scheduler?: Scheduler | Function;
+    debugger?: Function;
+    lazy?: boolean;
   }
 
-  function observe<Reaction extends Function>(func: Reaction, options?: ObserveOptions): Reaction
-  function unobserve(func: Function): void
-  function startTransaction(identity: any): void
-  function endTransaction(identity: any): void
-  function withTransaction<F extends Function>(fn: F): F
-  function withTransaction(target: any, key: any, d: TypedPropertyDescriptor<any>): void;
+  function observe<Reaction extends Function>(
+    func: Reaction,
+    options?: ObserveOptions
+  ): Reaction;
+  function unobserve(func: Function): void;
+  function startTransaction(identity: any): void;
+  function endTransaction(identity: any): void;
+  function withTransaction<F extends Function>(fn: F): F;
+  function withTransaction(
+    target: any,
+    key: any,
+    d: TypedPropertyDescriptor<any>
+  ): void;
+  function config(arg: ObservableConfig): ObservableConfig;
+  function action<F extends Function>(fn: F): F;
+  function action(target: any, key: any, d: TypedPropertyDescriptor<any>): void;
 }
