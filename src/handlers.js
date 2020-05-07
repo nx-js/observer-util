df7453437c295ae069b43730626689b37ab6b63b
Miklos Bertalan <miklos.bertalan@risingstack.com>
2020-05-07 10:23:14 +0200

    feat(handlers): add custom handlers

--- src/handlers.js
+++ src/handlers.js
@@ -1,81 +1,96 @@
-import { observable } from './observable'
-import { proxyToRaw, rawToProxy } from './internals'
+import { observable } from "./observable";
+import { proxyToRaw, rawToProxy, rawToHandlers } from "./internals";
 import {
   registerRunningReactionForOperation,
   queueReactionsForOperation,
   hasRunningReaction
-} from './reactionRunner'
+} from "./reactionRunner";
 
-const hasOwnProperty = Object.prototype.hasOwnProperty
+const hasOwnProperty = Object.prototype.hasOwnProperty;
 const wellKnownSymbols = new Set(
   Object.getOwnPropertyNames(Symbol)
     .map(key => Symbol[key])
-    .filter(value => typeof value === 'symbol')
-)
+    .filter(value => typeof value === "symbol")
+);
+
+function getHandlers(target) {
+  return rawToHandlers.get(target) || Reflect;
+}
 
 // intercept get operations on observables to know which reaction uses their properties
-function get (target, key, receiver) {
-  const result = Reflect.get(target, key, receiver)
+function get(target, key, receiver) {
+  const handlers = getHandlers(target);
+  const result = handlers.get(target, key, receiver);
   // do not register (observable.prop -> reaction) pairs for well known symbols
   // these symbols are frequently retrieved in low level JavaScript under the hood
-  if (typeof key === 'symbol' && wellKnownSymbols.has(key)) {
-    return result
+  if (typeof key === "symbol" && wellKnownSymbols.has(key)) {
+    return result;
   }
   // register and save (observable.prop -> runningReaction)
-  registerRunningReactionForOperation({ target, key, receiver, type: 'get' })
-  // if we are inside a reaction and observable.prop is an object wrap it in an observable too
+  registerRunningReactionForOperation({ target, key, receiver, type: "get" });
+
+  const observableResult = rawToProxy.get(result);
+
+  // if we are inside a reaction and observable.prop is an object, wrap it in an observable too
   // this is needed to intercept property access on that object too (dynamic observable tree)
-  const observableResult = rawToProxy.get(result)
-  if (hasRunningReaction() && typeof result === 'object' && result !== null) {
+  // always wrap when there are custom handlers (even when there is no running reaction)
+  // custom handlers might do things outside of reactions so they need this
+  if (
+    (hasRunningReaction() || handlers !== Reflect) &&
+    typeof result === "object" &&
+    result !== null
+  ) {
     if (observableResult) {
-      return observableResult
+      return observableResult;
     }
     // do not violate the none-configurable none-writable prop get handler invariant
     // fall back to none reactive mode in this case, instead of letting the Proxy throw a TypeError
-    const descriptor = Reflect.getOwnPropertyDescriptor(target, key)
+    const descriptor = Reflect.getOwnPropertyDescriptor(target, key);
     if (
       !descriptor ||
       !(descriptor.writable === false && descriptor.configurable === false)
     ) {
-      return observable(result)
+      // pass the parent's custom handlers to the child object
+      // this creates a 'deep proxy' which shares handlers deeply with its object children
+      return observable(result, handlers);
     }
   }
   // otherwise return the observable wrapper if it is already created and cached or the raw object
-  return observableResult || result
+  return observableResult || result;
 }
 
-function has (target, key) {
-  const result = Reflect.has(target, key)
+function has(target, key) {
+  const result = getHandlers(target).has(target, key);
   // register and save (observable.prop -> runningReaction)
-  registerRunningReactionForOperation({ target, key, type: 'has' })
-  return result
+  registerRunningReactionForOperation({ target, key, type: "has" });
+  return result;
 }
 
-function ownKeys (target) {
-  registerRunningReactionForOperation({ target, type: 'iterate' })
-  return Reflect.ownKeys(target)
+function ownKeys(target) {
+  registerRunningReactionForOperation({ target, type: "iterate" });
+  return getHandlers(target).ownKeys(target);
 }
 
 // intercept set operations on observables to know when to trigger reactions
-function set (target, key, value, receiver) {
+function set(target, key, value, receiver) {
   // make sure to do not pollute the raw object with observables
-  if (typeof value === 'object' && value !== null) {
-    value = proxyToRaw.get(value) || value
+  if (typeof value === "object" && value !== null) {
+    value = proxyToRaw.get(value) || value;
   }
   // save if the object had a descriptor for this key
-  const hadKey = hasOwnProperty.call(target, key)
+  const hadKey = hasOwnProperty.call(target, key);
   // save if the value changed because of this set operation
-  const oldValue = target[key]
+  const oldValue = target[key];
   // execute the set operation before running any reaction
-  const result = Reflect.set(target, key, value, receiver)
+  const result = getHandlers(target).set(target, key, value, receiver);
   // do not queue reactions if the target of the operation is not the raw receiver
   // (possible because of prototypal inheritance)
   if (target !== proxyToRaw.get(receiver)) {
-    return result
+    return result;
   }
   // queue a reaction if it's a new property or its value changed
   if (!hadKey) {
-    queueReactionsForOperation({ target, key, value, receiver, type: 'add' })
+    queueReactionsForOperation({ target, key, value, receiver, type: "add" });
   } else if (value !== oldValue) {
     queueReactionsForOperation({
       target,
@@ -83,23 +98,23 @@ function set (target, key, value, receiver) {
       value,
       oldValue,
       receiver,
-      type: 'set'
-    })
+      type: "set"
+    });
   }
-  return result
+  return result;
 }
 
-function deleteProperty (target, key) {
+function deleteProperty(target, key) {
   // save if the object had the key
-  const hadKey = hasOwnProperty.call(target, key)
-  const oldValue = target[key]
+  const hadKey = hasOwnProperty.call(target, key);
+  const oldValue = target[key];
   // execute the delete operation before running any reaction
-  const result = Reflect.deleteProperty(target, key)
+  const result = getHandlers(target).deleteProperty(target, key);
   // only queue reactions for delete operations which resulted in an actual change
   if (hadKey) {
-    queueReactionsForOperation({ target, key, oldValue, type: 'delete' })
+    queueReactionsForOperation({ target, key, oldValue, type: "delete" });
   }
-  return result
+  return result;
 }
 
-export default { get, has, ownKeys, set, deleteProperty }
+export default { get, has, ownKeys, set, deleteProperty };
--- src/observable.js
+++ src/observable.js
@@ -1,34 +1,38 @@
-import { proxyToRaw, rawToProxy } from './internals'
-import { storeObservable } from './store'
-import * as builtIns from './builtIns'
-import baseHandlers from './handlers'
+import { proxyToRaw, rawToProxy, rawToHandlers } from "./internals";
+import { storeObservable } from "./store";
+import * as builtIns from "./builtIns";
+import baseHandlers from "./handlers";
 
-export function observable (obj = {}) {
+export function observable(obj = {}, customHandlers) {
   // if it is already an observable or it should not be wrapped, return it
   if (proxyToRaw.has(obj) || !builtIns.shouldInstrument(obj)) {
-    return obj
+    return obj;
+  }
+  // add customHandlers to the raw object
+  if (customHandlers) {
+    rawToHandlers.set(obj, customHandlers);
   }
   // if it already has a cached observable wrapper, return it
   // otherwise create a new observable
-  return rawToProxy.get(obj) || createObservable(obj)
+  return rawToProxy.get(obj) || createObservable(obj, customHandlers);
 }
 
-function createObservable (obj) {
+function createObservable(obj, customHandlers) {
   // if it is a complex built-in object or a normal object, wrap it
-  const handlers = builtIns.getHandlers(obj) || baseHandlers
-  const observable = new Proxy(obj, handlers)
+  const handlers = builtIns.getHandlers(obj) || baseHandlers;
+  const observable = new Proxy(obj, { ...customHandlers, ...handlers });
   // save these to switch between the raw object and the wrapped object with ease later
-  rawToProxy.set(obj, observable)
-  proxyToRaw.set(observable, obj)
+  rawToProxy.set(obj, observable);
+  proxyToRaw.set(observable, obj);
   // init basic data structures to save and cleanup later (observable.prop -> reaction) connections
-  storeObservable(obj)
-  return observable
+  storeObservable(obj);
+  return observable;
 }
 
-export function isObservable (obj) {
-  return proxyToRaw.has(obj)
+export function isObservable(obj) {
+  return proxyToRaw.has(obj);
 }
 
-export function raw (obj) {
-  return proxyToRaw.get(obj) || obj
+export function raw(obj) {
+  return proxyToRaw.get(obj) || obj;
 }
